94b13e31ef8a69b9bbd341615f40e9fcd1c186b3
Shoes <shooo3sli@gmail.com>
2020-06-11 18:27:03 +0800

    modified gitignore to stop tracking constants.swift, added constantstemplate.swift instead

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Shoes.s.Li on 2020/6/11.
+//  Copyright © 2020 Shoes.s.Li. All rights reserved.
+//
+/*
+ Add your own keys below and uncomment the code
+ */
+
+import Foundation
+
+/*
+struct ConstantsTemplate {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
